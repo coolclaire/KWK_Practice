6a14653e1504ba034d7d1b6a1e3adf00fe007322
Test Name <test@gmail.com>
2021-07-16 00:27:34 -0700

    Initial Commit

---
+++ KWK_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KWK_Practice
+//
+//  Created by Christine Agno on 7/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
